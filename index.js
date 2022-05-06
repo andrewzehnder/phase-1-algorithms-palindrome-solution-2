4f86f5f9329fc51eefccfca1e04ab6784c340d1b
Andrew <azehnder2@gmail.com>
2022-05-06 15:37:18 -0700

    commit tree a8c244e57dc9dfdde5cee01ee8662e76d0f4e4d4

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+Create a function with the inputted word.
+Check the length of the word so you can compare the first and the last letters. Knowing where to stop the comparison. 
+Compare the first letter and last letter to see if they match.
+Loop through this for every letter.
+If it's an odd length, no need to compare the middle letter.
+If every letter matches, return true.
+If one set of comparisons fail, return false.
 */
 
 /*
-  Add written explanation of your solution here
+  The function needs to take in a word and needs to output whether the word is a palindrome. 
+  If not, it needs to output false.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
